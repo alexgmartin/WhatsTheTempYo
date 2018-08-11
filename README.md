485d86fe639e8ef9cf58872e943ac2f7f79c31a2
Alex Martin <alexgm88@gmail.com>
2018-08-11 16:45:50 -0400

    reset file transfer 2

---
+++ README.md
@@ -0,0 +1,3 @@
+# What's The Temp Yo?
+
+A NodeJs App
---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const request = require('request');
+const path = require('path');
+const dotenv = require('dotenv').config();
+const PORT = process.env.PORT || 5000;
+const apiKey = '37e2ea2bec5412f7042c404f48d951df';
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .use(bodyParser.urlencoded({ extended: true }))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) =>
+    res.render('pages/index', { weather: null, error: null })
+  )
+  .post('/', function(req, res) {
+    let city = req.body.city;
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+
+    request(url, function(err, response, body) {
+      if (err) {
+        res.render('pages/index', {
+          weather: null,
+          error: 'Error, please try again'
+        });
+      } else {
+        let weather = JSON.parse(body);
+        if (weather.main == undefined) {
+          res.render('pages/index', {
+            weather: null,
+            error: 'Error, please try again'
+          });
+        } else {
+          let weatherText = `It's ${weather.main.temp} degrees in ${
+            weather.name
+          }!`;
+          res.render('pages/index', { weather: weatherText, error: null });
+        }
+      }
+    });
+  })
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
